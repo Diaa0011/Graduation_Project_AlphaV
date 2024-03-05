65393d0cb9d5bf2c9619767eb307ad4a6b443f5e
MazenDarweesh <mazentharwatfathy@gmail.com>
2024-03-05 15:33:45 +0200

    BaseRepo

--- GraduationProjectAlpha/Entities/BaseEntity.cs
+++ GraduationProjectAlpha/Entities/BaseEntity.cs
@@ -1,7 +1,12 @@
-﻿namespace GraduationProjectAlpha.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraduationProjectAlpha.Entities
 {
 	public class BaseEntity
 	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 		public string CreatedBy { get; set; } = "Admin";
