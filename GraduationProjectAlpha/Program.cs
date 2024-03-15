e33478ac619bb0ab2c5b0031dd52859b3ba40a20
Shehab-Eldeen <shehabeldeen1995@gmail.com>
2024-03-15 10:41:39 +0200

    add cors policy

--- GraduationProjectAlpha/Program.cs
+++ GraduationProjectAlpha/Program.cs
@@ -13,6 +13,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowCredentials();
+    });
+});
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
     {
         options.Password.RequiredLength = 5;
@@ -56,6 +67,7 @@ if (app.Environment.IsDevelopment()){
 
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapRazorPages();
